bdd3dcb19420db6eb83f8e3721ab89db91f7752f
Lynn524552751 <524552751@qq.com>
2018-02-03 19:01:22 +0800

    update

--- wxFlynn-part/test/test.js
+++ wxFlynn-part/test/test.js
@@ -7,12 +7,10 @@ Page({
   data: {
     name: 'test',
     loading: true,
-    loadMore: true,
     api: {
       url: 'http://s.music.qq.com/fcgi-bin/music_search_new_platform?t=0& amp;n={2}&aggr=1&cr=1&loginUin={3}&format=json&inCharset=GB2312&outCharset=utf-8&notice=0&platform=jqminiframe.json&needNewCode=0&p={1}&catZhida=0&remoteplace=sizer.newclient.next_song&w={0}', 
     },
     list: [],
-    img: '../../static/images/douban.jpg',
     page: 1,
     per: 3 * 5,
   },
@@ -26,15 +24,6 @@ Page({
       return this.updateData()
     }
     return this.setShowData(data)
-  },
-  getTags: function () {
-    sysUtil.http.get(this.data.api.tagUrl)
-      .then(result => {
-        console.log(result)
-      })
-      .catch(e => {
-        console.error(e)
-      })
   },
   updateData: function () {
     var url = this.data.api.url.replace("{0}", "周杰伦").replace("{1}", "1").replace("{2}", "25").replace("{3}", "0")
@@ -52,12 +41,11 @@ Page({
       })
   },
   setShowData: function (data) {
-    console.log(data)
+    var list = data.data.song.list
+    console.log(list)
     this.setData({
       list: list,
       loading: false,
-      page: page,
-      loadMore: loadMore
     })
   },
   onReachBottom: function () {
