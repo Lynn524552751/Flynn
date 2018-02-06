bf79a0be1ddc07826fbc38150dfb65e446c19b3b
Lynn524552751 <524552751@qq.com>
2018-02-06 21:42:55 +0800

    update

--- wxFlynn-part/douban/douban.js
+++ wxFlynn-part/douban/douban.js
@@ -5,32 +5,29 @@ const sysUtil = require('../../utils/sysUtil.js')
 // console.log(decodeURI("%E7%88%B1%E6%83%85"))
 Page({
   data: {
-    name: 'test',
+    name: 'douban',
     loading: true,
     loadMore: true,
     api: {
       url: 'https://movie.douban.com/j/search_subjects',
     },
     list: [],
-    tag: ["热门", "国产剧", "综艺", "美剧", "日剧", "韩剧", "日本动画", "纪录片"],
+    tags: [],
     page: 0,
     per: 3 * 5,
     activeId: 0,
   },
   onLoad: function (options) {
     var title = ""
+    var tags = []
     switch (options.name) {
       case "movie":
         title = "电影"
-        this.updateData("movie")
+        tags = ["热门", "最新", "豆瓣高分", "冷门佳片", "华语", "欧美", "韩国", "日本"]
         break;
       case "tv":
         title = "电视剧"
-        this.updateData("tv")
-        break;
-      case "variety":
-        title = "综艺"
-        this.updateData("tv","综艺")
+        tags = ["热门", "国产剧", "综艺", "美剧", "日剧", "韩剧", "日本动画", "纪录片"]   
         break;
       default:
         break
@@ -38,6 +35,12 @@ Page({
     wx.setNavigationBarTitle({
       title: title
     })
+    this.setData({
+      name: options.name,
+      tags: tags,
+      activeId:0
+    })
+    this.updateData(this.data.name)
   },
   getData: function () {
     var data = wx.getStorageSync(this.data.name)
@@ -56,48 +59,48 @@ Page({
         console.error(e)
       })
   },
-  updateData: function (type, tag_index = 0) {
+  updateData: function (name,index=0) {
     var url = this.data.api.url
+    var tag = this.data.tags[index]
     var data = {
-      type: type,
-      tag: this.data.tag[tag_index],
+      type: name,
+      tag: tag,
       page_limit:50,
       page_start:0
     }
     sysUtil.http.get(url,data)
       .then(result => {
-        console.log(result)
+        console.log(result.statusCode)
         result.data.save_time = sysUtil.dateFormat(new Date(), "MM-dd")
         wx.setStorageSync(this.data.name, result.data)
         if (this.setShowData) {
-          this.setShowData(result.data)
+          this.setShowData(result.data,false, index)
         }
       })
       .catch(e => {
         console.error(e)
       })
   },
-  setShowData: function (data) {
-    var page = this.data.page + 1
-    var list = data.subjects
-    var loadMore = list.length <= page * this.data.per
-    list = list.slice(0, page * this.data.per)
-    console.log(list)
+  setShowData: function (data, more, index) {
+    var page = more ? this.data.page + 1 : 1 
+    var list = data.subjects.slice(0, page * this.data.per)
+    var loadMore = data.subjects.length <= page * this.data.per   
+    console.log(list.length)
     this.setData({
       list: list,
       loading: false,
       page: page,
-      loadMore: loadMore
+      loadMore: loadMore,
+      activeId: index
     })
   },
   onReachBottom: function () {
     if (this.data.loadMore) { return }
     setTimeout(e=>{
       if (this.setShowData) {
-        this.setShowData(wx.getStorageSync(this.data.name))
+        this.setShowData(wx.getStorageSync(this.data.name), true, this.data.activeId)
       }
     },500)
-    
   },
   previewImage: function (e) {
     wx.previewImage({
@@ -113,10 +116,7 @@ Page({
   },
   activeTab: function (e) {
     var index = e.currentTarget.dataset.index
-    this.updateData("tv", index)
-    this.setData({
-      activeId: index
-    })
-
+    console.log(index)
+    this.updateData(this.data.name, index)
   },
 })
