beac30cf01a3830eac0a81088e35ecf6c6f69547
Lynn524552751 <524552751@qq.com>
2018-02-19 15:17:28 +0800

    update

--- wxFlynn-part/index/index-def.js
+++ wxFlynn-part/index/index-def.js
@@ -8,6 +8,7 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
   bindViewTap: function() {
@@ -27,22 +28,29 @@ Page({
       url: url
     })
   },
-  onLoad: function () {  
+  onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    }else {     
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
-          })
-          wx.setNavigationBarTitle({
-            title: this.data.userInfo.nickName
           })
         }
       })
