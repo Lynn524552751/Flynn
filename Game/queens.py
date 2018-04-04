98a72069b24ce05688afe9e15fff22c7c9442ec1
Lynn524552751 <524552751@qq.com>
2018-04-04 20:44:18 +0800

    update

--- Game/queens.py
+++ Game/queens.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 
+sum = 0
+
 def queen(A, cur=0):
     if cur == len(A):
         print(A)
+        global sum
+        sum = sum+1
         return 0
-    #col == 列
+    #遍历所有列
     for col in range(len(A)):
         A[cur], flag = col, True
-        # row == 行
+        #遍历当前行之前所有行
         for row in range(cur):
             #行冲突 or 斜冲突
-            if A[row] == col or abs(col - A[row]) == cur - row:
+            if abs(col - A[row]) in (0, cur - row):
                 flag = False
                 break
         if flag:
+            # 下一行
             queen(A, cur+1)
 
 queen([None]*8)
+print(sum)
